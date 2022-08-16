5f1569b68a2ef1e93c4e54c838f8a92aa332fbe1
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-08-16 13:42:38 +1000

    add item done

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,8 +1,24 @@
+import React,{useState} from 'react';
 import './App.css';
+import TodoBoard from './component/TodoBoard';
 
+
+//input text, add button
+//item delete button
 function App() {
+  const [inputValue,setInputValue] = useState('')
+  const [todoList,setTodoList]=useState([])
+  const addItem = () => {
+    console.log("I'm here",inputValue)
+    setTodoList([...todoList,inputValue])
+  }
   return (
-
+    <main>
+      <input value={inputValue} type='text' onChange=
+      {(event)=>setInputValue(event.target.value)}/>
+      <button onClick={addItem}>Add</button>
+      <TodoBoard todoList={todoList}/>
+    </main>
   );
 }
 
